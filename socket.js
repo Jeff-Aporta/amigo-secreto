3cfe60e328a52e4d7c4a6b523114f9b1cff41bd3
Jeff-Aporta <73729410+Jeff-Aporta@users.noreply.github.com>
2021-11-22 12:07:37 -0500

    Update socket.js

--- socket.js
+++ socket.js
@@ -2,55 +2,63 @@ const nodemailer = require("nodemailer");
 
 module.exports = function (io, app) {
   io.on("connection", (socket) => {
-    console.log("nuevo usuario conectado: " + socket.id);
-
-    socket.on("disconnect", function () {
-      console.log("Usuario desconectado: " + socket.id);
-    });
-
-    socket.on("enviar correos", (lista) => {
-      let jugadores = lista[0];
-      let parejas = lista[1];
+    //Se ha conectado un nuevo usuario
+    socket.on("enviar correos", (jugadores, parejas) => {
       for (let i = 0; i < jugadores.length; i++) {
-        let jugador = jugadores[i];
-        let pareja = parejas[i];
-        console.log(jugador[0] + " : " + pareja[0]);
-        console.log(jugador[1] + " : " + pareja[1]);
-        enviarMail(
-          jugador[1],
-          "Amigo secreto",
-          `<h4>Hola ${jugador[0]}</h4> Tu amigo secreto es: ${pareja[0]}`
-        );
+        const j = jugadores[i];
+        const p = parejas[i];
+        console.log(j[0] + " -> " + p[0]);
+        console.log(j[1] + " -> " + p[1]);
+        setTimeout(() => { //protocolo del gestor de envios
+          enviarCorreo(
+            j[1], //correo
+            "Amigo secreto", //Asunto
+            `<h4>Hola ${j[0]}</h4>Tu amigo secreto es: ${p[0]} (${p[1]})<br><span style="color:lightgray">No reveles esta información a otra persona</span>` //Cuerpo del mensaje (html)
+          );
+        }, 1000 * i);
       }
     });
 
-    function enviarMail(to, subject, html) {
-      const transporter = nodemailer.createTransport({
-        host: "smtp-mail.outlook.com",
-        secureConnection: false,
-        port: 587,
-        tls: {
-          ciphers: "SSLv3",
-        },
-        auth: {
-          user: "no-reply-3@outlook.com",
-          pass: "Jeffrey1.618",
-        },
-      });
-      var mailOptions = {
-        from: '"No responder" no-reply-3@outlook.com',
-        to,
-        subject: subject + " " + Math.floor(Math.random() * 1000),
-        html,
-      };
-      transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-          io.to(socket.id).emit("correo no enviado", error);
-          console.log(error);
-        }else{
-          io.to(socket.id).emit("correo enviado", to);
-        }
-      });
+    conexiones_correo = 0;
+
+    function enviarCorreo(para, asunto, html) {
+      //Tienen que crearse un correo @outlook, @hotmail funciona pero tiene menos cobertura
+      if (conexiones_correo == 0) { //Protocolo del gestor de envios
+        conexiones_correo++;
+        const transporter = nodemailer.createTransport({
+          host: "smtp-mail.outlook.com",
+          secureConnection: false,
+          port: 587,
+          tls: {
+            ciphers: "SSLv3",
+          },
+          auth: {
+            user: "no-reply-3@outlook.com",
+            pass: "pass123word.",
+          },
+        });
+        const correo_opns = {
+          from: '"No responder" no-reply-3@outlook.com',
+          to: para,
+          subject: asunto + " " + Math.floor(Math.random() * 1000),
+          html,
+        };
+        console.log("enviando correo a: " + para);
+        transporter.sendMail(correo_opns, (error, info) => {
+          conexiones_correo--;
+          if (error) {
+            io.to(socket.id).emit("correo no enviado", error);
+            console.log(error);
+          } else {
+            io.to(socket.id).emit("correo enviado", para);
+            console.log("correo enviado a : " + para);
+          }
+        });
+      } else {
+        setTimeout(() => {//protocolo del gestor de envios
+          enviarCorreo(para, asunto, html);
+        }, 1000);
+      }
     }
   });
 };
